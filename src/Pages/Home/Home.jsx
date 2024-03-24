c4aeb000e3b4d2e8b765346bb06a4d69e58b8548
devenamulshikder <devenamulshikder@gmail.com>
2024-03-24 16:20:05 +0600

    all data fetch with async await

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../../components/Banner/Banner";
+import CategoryList from "../../components/CategoryList/CategoryList";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <CategoryList></CategoryList>
     </div>
   );
 };
---
+++ src/components/CategoryList/CategoryList.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import useDonationData from '../../Hooks/useDonationData'
+
+const CategoryList = () => {
+    const {data, loading} = useDonationData();
+    console.log(data);
+  return (
+    <div>
+        <h2>Category......</h2>
+    </div>
+  )
+}
+
+export default CategoryList
